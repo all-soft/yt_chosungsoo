bb4d1e80f36091ef96aab36b282e212b9a15af78
yang <goldstone.yang@gmail.com>
2018-12-31 20:28:09 +0900

    광고 ID 수정

--- app/src/main/java/com/chsapps/yt_nahoonha/constants/AdConstants.java
+++ app/src/main/java/com/chsapps/yt_nahoonha/constants/AdConstants.java
@@ -1,7 +1,7 @@
 package com.chsapps.yt_nahoonha.constants;
 
 public class AdConstants {
-    public static final String AD_ID_BANNER = "ca-app-pub-5539000902582500/9158582871";
-    public static final String AD_ID_INTERSTITIAL = "ca-app-pub-5539000902582500/2593174523";
+    public static final String AD_ID_BANNER = "ca-app-pub-5539000902582500/5307851592";
+    public static final String AD_ID_INTERSTITIAL = "ca-app-pub-5539000902582500/7368050483";
     public static final String AD_ID_NATIVE = "ca-app-pub-5539000902582500/5397608635";
 }
